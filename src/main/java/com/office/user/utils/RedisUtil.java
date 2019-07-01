2242dd94fc25441917fe5a5343edf6ba0e481b59
Cc <li_chao_cheng@163.com>
2019-07-01 14:36:08 +0800

    2019-07-01:14 update redis

--- src/main/java/com/office/user/utils/RedisUtil.java
+++ src/main/java/com/office/user/utils/RedisUtil.java
@@ -1,11 +1,6 @@
 package com.office.user.utils;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.redis.core.StringRedisTemplate;
-import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.stereotype.Repository;
-
-import java.util.concurrent.TimeUnit;
 
 /**
  * @author Hyper-Li
